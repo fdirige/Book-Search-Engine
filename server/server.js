4c25efbe838f6917421250114ee9f843141ae630
fdirige <118780727+fdirige@users.noreply.github.com>
2023-05-17 22:02:00 -0700

    fixed bugs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { ApolloServer } = require('@apollo/server');
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
@@ -16,8 +16,6 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -37,9 +35,13 @@ app.get("*", (req, res) => {
 
 // app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+server.start().then(res => {
+  server.applyMiddleware({ app });
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
   });
 });
+
